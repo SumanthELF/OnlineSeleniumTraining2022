69935aa27634bcab9a4790f3bfeedb46b16d15b8
Sumanth <Sumanth@TYSS-Sumanth>
2022-09-14 17:54:57 +0530

    added new LatestScript.java, TC_Organisation_006.java and modified TC_Organisation_001.java

---
+++ OnlineSelenium/src/newPackage/LatestScript.java
@@ -0,0 +1,12 @@
+package newPackage;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class LatestScript {
+
+	@Test
+	public void add() {
+		Reporter.log("Hi How are you",true);
+	}
+}
--- OnlineSelenium/src/organisation/TC_Organisation_006.java
+++ OnlineSelenium/src/organisation/TC_Organisation_006.java
@@ -5,21 +5,20 @@ import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
-
 /***
  * 
  * @author Sumanth
  *
  */
-public class TC_Organisation_001 { //TestNG class  // test case id
+public class TC_Organisation_006 {   // test case id
 
-	@Test(groups= {"smoke","org"})
-	public void createAccount() {  //test case  // test case name
+	@Test
+	public void modifyAccount() {  // test case name
 		System.setProperty("webdriver.chrome.driver", "./drivers/chromedriver.exe");
-		WebDriver driver = new ChromeDriver();                        //test steps
+		WebDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-		driver.get("https://www.samsung.com/");
+		driver.get("https://www.sony.co.in/");
 		driver.quit();
 	}
 }
